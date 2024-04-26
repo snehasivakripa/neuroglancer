b3b96b413b9ce5be81dde91fea4b055aae963511
Sneha B <55646649+snehasivakripa@users.noreply.github.com>
2024-04-25 20:03:04 -0400

    Update nifti_zar_transformation.ts

--- src/datasource/nifti_zar_transformation/nifti_zar_transformation.ts
+++ src/datasource/nifti_zar_transformation/nifti_zar_transformation.ts
@@ -4,12 +4,24 @@ import {
   VolumeSourceParameters,
 } from "#src/datasource/nifti/base.js";
 import zarr_base = require("src/datasource/zarr/base.ts")
+import zarr_backend = require("src/datasource/zarr/backend.ts")
+import type { VolumeChunk } from "#src/sliceview/volume/backend.js";
+
+
+
 
 //Call to registerPromiseRPC and passing nifti_volume_rpc_id to transform it
-var nifti = new nifti_backend.registerPromiseRPC(GET_NIFTI_VOLUME_INFO_RPC_ID);
+const nifti = new nifti_backend.registerPromiseRPC(GET_NIFTI_VOLUME_INFO_RPC_ID);
 
 //Getting zarr_volume_rpc_id
-var GET_ZARR_VOLUME_INFO_RPC_ID =new zarr_base.RPC_ID;
+const GET_ZARR_VOLUME_INFO_RPC_ID =new zarr_base.RPC_ID;
 
 //Call to registerPromiseRPC and passing zarr_volume_rpc_id to transform it
-var zarr = new nifti_backend.registerPromiseRPC(GET_ZARR_VOLUME_INFO_RPC_ID);
+const zarr = new nifti_backend.registerPromiseRPC(GET_ZARR_VOLUME_INFO_RPC_ID);
+
+//Call to class ZarrVolumeChunkSource
+//Initializing parameter cancellation token
+// Call to download function in zarr.backend.ts to convert to zarr format
+const zarrVolumeChunkSource = new zarr_backend.ZarrVolumeChunkSource();
+const cancellationToken = new CancellationTokenSource();
+zarrVolumeChunkSource.download(zarrVolumeChunkSource, cancellationToken);
